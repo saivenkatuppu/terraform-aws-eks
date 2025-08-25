af9d6ca603cfe27ee8cd3931f07c5372eba0a190
sai <saivenkat67687@gmail.com>
2025-08-25 19:26:29 +0530

    cicd-tools-main

--- 60-acm/parameters.tf
+++ 60-acm/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "acm_certificate_arn" {
   name  = "/${var.project}/${var.environment}/acm_certificate_arn"
   type  = "String"
-  value = aws_acm_certificate.daws84s.arn
+  value = aws_acm_certificate.calvio.arn
 }
