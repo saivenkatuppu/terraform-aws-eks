277b7734060ecd1f03b638c36e61d4e925cfe6d4
sai <saivenkat67687@gmail.com>
2025-08-25 19:03:55 +0530

    cicd-tools-main

--- 20-bastion/provider.tf
+++ 20-bastion/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "devops84s-practice"
-    key    = "vpc-demo1"
+    key    = "vpc-demo-bastian"
     region = "us-east-1"
     encrypt        = true
     use_lockfile = true  #----> locking the state file 
